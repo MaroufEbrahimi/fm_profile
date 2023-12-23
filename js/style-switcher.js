0584d87a1d20b44b9c4798d29a40a7ecd38632b8
Marouf <maroufibrahimi7264@gmail.com>
2023-12-23 19:06:49 +0430

    complete style switcher

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -22,3 +22,18 @@ function setActiveStyle(color) {
     }
   });
 }
+
+/*  __________  Theme light & dark __________ */
+const dayNight = document.querySelector(".day_night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
