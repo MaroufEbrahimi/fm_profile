7a6cae3bf99cd13b4caef928ad05c9b4f205c21a
Marouf <maroufibrahimi7264@gmail.com>
2023-12-23 17:50:12 +0430

    js for Theme Colors

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -11,4 +11,14 @@ window.addEventListener("scroll", () => {
   }
 });
 
-
+/*  __________  Theme Colors __________ */
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+  alternateStyles.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
