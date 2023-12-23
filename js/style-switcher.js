e9ed52cdc8eb9c0ef650567469809927b23807c4
Marouf <maroufibrahimi7264@gmail.com>
2023-12-23 17:49:59 +0430

    js for hide switcher

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -4,4 +4,11 @@ styleSwitcherToggle.addEventListener("click", () => {
   document.querySelector(".style_switcher").classList.toggle("open");
 });
 
+// hide style switcher on scroll
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style_switcher").classList.contains("open")) {
+    document.querySelector(".style_switcher").classList.remove("open");
+  }
+});
+
 
