5668335fd4437c31c732cfc5b08d47430ffd66f1
Marouf <maroufibrahimi7264@gmail.com>
2023-12-26 00:09:03 +0430

    displaying every section

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ var typed = new Typed(".typing", {
 /*  __________ Asid Links Navigate __________ */
 const nav = document.querySelector(".nav"),
   navList = nav.querySelectorAll("li"),
-  totalNavList = navList.length;
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
 
 for (let i = 0; i < totalNavList; i++) {
   const a = navList[i].querySelector("a");
@@ -18,5 +20,14 @@ for (let i = 0; i < totalNavList; i++) {
       navList[j].querySelector("a").classList.remove("active");
     }
     this.classList.add("active");
+    showSection(this);
   });
+}
+
+function showSection(ele) {
+  for (let a = 0; a < totalSection; a++) {
+    allSection[a].classList.remove("active");
+  }
+  const target = ele.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
 }
