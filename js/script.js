087f8a8cae3eac51f725990abb42cd31597b1578
Marouf <maroufibrahimi7264@gmail.com>
2023-12-25 23:49:14 +0430

    links navigate

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ var typed = new Typed(".typing", {
   backSpeed: 50,
   loop: true,
 });
+
+/*  __________ Asid Links Navigate __________ */
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length;
+
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let j = 0; j < totalNavList; j++) {
+      navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
